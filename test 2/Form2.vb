ee6520fcdd9bb2a1734cd618ac36f68dea98df0e
salma <salma@SALMAN>
2026-02-06 07:27:53 +0700

    test

--- test 2/Form2.vb
+++ test 2/Form2.vb
@@ -4,7 +4,7 @@
     End Sub
 
     Private Sub Label2_Click(sender As Object, e As EventArgs) Handles lblPassword.Click
-
+        "test"
     End Sub
 
     Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles Label1.Click
